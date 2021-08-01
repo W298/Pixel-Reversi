611bf614e20972fb509ad94626b6363cc318719d
ruka <lutca1320@gmail.com>
2021-08-01 21:14:26 +0900

    Fix Screen Error & Init to New Repo

---
+++ Assets/Scripts/DynamicCamera.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicCamera : MonoBehaviour
+{
+    private Camera camera = null;
+    private float initialValue;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = this.gameObject.GetComponent<Camera>();
+        initialValue = camera.orthographicSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Screen.width <= Screen.height)
+        {
+            camera.orthographicSize = Convert.ToSingle(initialValue * ((float)Screen.height / (float)Screen.width) * 0.8);
+        }
+    }
+}
